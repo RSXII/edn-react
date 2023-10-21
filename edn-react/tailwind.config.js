b31ae1ceb5a080921792e9c30b6726b8e9052cfc
Ryan Schultz <rdschultz@verisk.com>
2023-10-21 07:40:26 -0600

    setup basic layout and prepare to split components

---
+++ edn-react/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      buttonPrimary: "#324A43",
+      buttonHover: "#3B7D6A",
+      buttonTextPrimary: "#FFFFFF",
+      backgroundColor: "#bbf7d0",
+    },
+  },
+  plugins: [],
+};
