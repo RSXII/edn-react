5dcaef84dc285a368005a70e013a3bac26128b9b
Ryan Schultz <rdschultz@verisk.com>
2023-10-23 07:58:38 -0600

    track current nav

--- edn-react/src/components/navigation/NavigationComponent.tsx
+++ edn-react/src/components/navigation/NavigationComponent.tsx
@@ -1,12 +1,18 @@
+import { useLocation } from "react-router-dom";
 import { Navigation } from "./types";
 export default function Navigation({ items }: { items: Navigation[] }) {
+  const location = useLocation();
   return (
     <div className="hidden lg:flex lg:gap-x-12">
       {items.map((item: Navigation) => (
         <a
           key={item.name}
           href={item.href}
-          className="text-sm font-semibold leading-6 text-gray-900"
+          className={`text-sm font-semibold leading-6 hover:text-teal-500 ${
+            location.pathname === item.href
+              ? "text-teal-800 font-extrabold"
+              : "text-gray-900"
+          } `}
         >
           {item.name}
         </a>
