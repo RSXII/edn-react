0bbf7acc0d7e9e28d9e928a8394b72e0c328b810
Ryan Schultz <rdschultz@verisk.com>
2023-10-30 08:57:15 -0600

    fix profile link in mobile menu when not logged in

--- edn-react/src/components/navigation/MobileMenuComponent.tsx
+++ edn-react/src/components/navigation/MobileMenuComponent.tsx
@@ -29,25 +29,31 @@ function LogOut() {
 
 export default function MobileMenu({ items }: { items: Navigation[] }) {
   const location = useLocation();
-  const { isLoaded, userId } = useAuth();
+  const { isLoaded, userId, isSignedIn } = useAuth();
   if (!isLoaded) return null;
   return (
     <div className="mt-6 flow-root">
       <div className="-my-6 divide-y divide-white">
         <div className="space-y-2 py-6">
-          {items.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 ${
-                location.pathname === item.href
-                  ? "text-teal-800 font-extrabold"
-                  : "text-gray-900"
-              } hover:bg-emerald-300`}
-            >
-              {item.name}
-            </a>
-          ))}
+          {items.map((item) => {
+            const shouldRenderItem = !item.protected || isSignedIn;
+            if (shouldRenderItem) {
+              return (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 ${
+                    location.pathname === item.href
+                      ? "text-teal-800 font-extrabold"
+                      : "text-gray-900"
+                  } hover:bg-emerald-300`}
+                >
+                  {item.name}
+                </a>
+              );
+            }
+            return null;
+          })}
         </div>
         <div className="py-6">{userId ? <LogOut /> : <LogIn />}</div>
       </div>
