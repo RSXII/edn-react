7e8dc6cdb240f60610f29a1b3665336bafc56d69
Ryan Schultz <rdschultz@verisk.com>
2023-11-10 18:41:22 -0700

    change allowed origins

--- edn-api/main.go
+++ edn-api/main.go
@@ -16,7 +16,7 @@ func main() {
     r := gin.Default()
 
     config := cors.DefaultConfig()
-    config.AllowOrigins = []string{"http://localhost:4200", "https://www.sarkaz.site"}
+    config.AllowOrigins = []string{"http://localhost:5173", "https://edn-react.vercel.app/"}
     config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 
     r.Use(cors.New(config))
